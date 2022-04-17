5ed297ec7bd7378d5760458342ec0ee3a38c93be
KIMTAEKWAN <twan4220@gmail.com>
2022-04-17 16:53:44 +0000

    mission

--- FAQ_model/models.py
+++ FAQ_model/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class support(models.Model):
+    title = models.TextField('제목')
+    content = models.TextField('내용')
+    image = models.ImageField(verbose_name= '이미지')
+    created_at = models.DateTimeField('작성일')
+    view_count = models.IntegerField('조회수')
--- mission/urls.py
+++ mission/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from FAQ_model.views import support
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('FAQ/', support)
 ]
