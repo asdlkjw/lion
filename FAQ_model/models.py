bd4d56c6a5152723848cdd4c7d3a6e343a42238b
KIMTAEKWAN <twan4220@gmail.com>
2022-04-18 16:04:30 +0000

    중간커밋

--- FAQ_model/models.py
+++ FAQ_model/models.py
@@ -7,17 +7,17 @@ User = get_user_model()
 
 # Create your models here.
 class support(models.Model):  #질문, 카테고리, 답변, 생성자, 생성일시, 최종 수정자, 최종 수정일시
-    title = models.TextField('질문')
-    category = models.CharField('카테고리', max_length=20)
-    content = models.TextField('답변')
-    create_user = models.CharField('생성자',max_length=20)
-    created_at = models.DateTimeField('생성일시')
-    last_user = models.CharField('최종 수정자',max_length=20)
-    last_at = models.DateTimeField('최종 수정일시')
-
+    title = models.TextField(verbose_name='질문')
+    category = models.CharField(verbose_name='카테고리', max_length=20)
+    content = models.TextField(verbose_name='답변')
+    create_user = models.CharField(verbose_name='생성자',max_length=20)
+    created_at = models.DateTimeField(verbose_name='생성일시', auto_now_add=True)
+    last_user = models.CharField(verbose_name='최종 수정자',max_length=20)
+    last_at = models.DateTimeField(verbose_name='최종 수정일시', auto_now_add=True)
+    writer = models.ForeignKey(to = User , on_delete=models.CASCADE, null= True, blank= True)
 
 class comment(models.Model):
     content = models.TextField(verbose_name='내용')
-    created_at = models.DateTimeField('작성일')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
     support = models.ForeignKey(to = 'support' , on_delete=models.CASCADE)
-    writer = models.ForeignKey(to = 'writer' , on_delete=models.CASCADE)
+    writer = models.ForeignKey(to = User , on_delete=models.CASCADE, null= True, blank= True)
--- FAQ_model/views.py
+++ FAQ_model/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+import FAQ_model.models 
 # Create your views here.
 def support(request):
-    return render(request, 'supports/support.html')
+
+    content = FAQ_model.models.support.objects.all()
+    for con in content:
+        print(con.content)
+
+        
+    return render(request, 'supports/support.html' )
