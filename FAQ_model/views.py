0e88ddf8f9f473e3f63178683c19df5d2ba622f2
KIMTAEKWAN <twan4220@gmail.com>
2022-04-19 13:06:23 +0000

    임시

--- FAQ_model/views.py
+++ FAQ_model/views.py
@@ -3,10 +3,11 @@ import FAQ_model.models
 # Create your views here.
 def support(request):
 
-    content = FAQ_model.models.support.objects.all()
-    for con in content:
-        print(con.content)
-
-        
-    # return render(request, 'supports/support.html' )
-    return render(request, 'supports/faq.html' )
+    db_all = FAQ_model.models.support.objects.all()
+    db_ = [db for db in db_all]
+    content = db_[0].content
+    title = db_[0].title
+    date = db_[0].created_at
+    return render(request, 'supports/support.html' , {'content': content, 'title': title, 'date': date,} )
+    
+    # return render(request, 'supports/faq.html' )
