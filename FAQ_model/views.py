669e02ad6310dfb624f182fbc667e9e8bff46477
KIMTAEKWAN <twan4220@gmail.com>
2022-04-19 13:18:20 +0000

    last

--- FAQ_model/views.py
+++ FAQ_model/views.py
@@ -5,9 +5,8 @@ def support(request):
 
     db_all = FAQ_model.models.support.objects.all()
     db_ = [db for db in db_all]
-    content = db_[0].content
-    title = db_[0].title
-    date = db_[0].created_at
-    return render(request, 'supports/support.html' , {'content': content, 'title': title, 'date': date,} )
-    
-    # return render(request, 'supports/faq.html' )
+    content = db_[-1].content
+    title = db_[-1].title
+    date = db_[-1].created_at
+    user = db_[-1].create_user
+    return render(request, 'supports/support.html' , {'content': content, 'title': title, 'date': date, 'user': user})
