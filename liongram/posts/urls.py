2e53c71fdf76f678a0488f721d50e3e5efa1dfe2
KIMTAEKWAN <twan4220@gmail.com>
2022-04-29 14:36:27 +0000

    up

--- liongram/posts/urls.py
+++ liongram/posts/urls.py
@@ -6,7 +6,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_view, name='post-list'),
-    path('new/', post_create_view, name='post-create'),
+    path('create/', post_create_view, name='post-create'), #templates에서 동적으로 변함 (name 을 지정해준곳에)
     path('<int:id>/', post_detail_view, name='post-detail'),
     path('<int:id>/edit/', post_update_view, name='post-update'),
     path('<int:id>/delete/', post_delete_view),
